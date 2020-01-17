9fd4d36cc29f6a23151c92a74543d9a3e56530ed
sncandiani <sofiancandiani@gmail.com>
2020-01-17 11:51:21 -0600

    implemented incrementing in js

---
+++ main.js
@@ -0,0 +1,10 @@
+let bandNumber = 1
+
+const takeNumber = function (bandName) {
+    console.log(`${bandNumber}. ${bandName}`)
+    bandNumber++;
+}
+
+takeNumber("Slowdive")
+takeNumber("Siouxsie and the Banshees")
+takeNumber("The Cure")
